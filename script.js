414b3daf874261168d6e89b16666ca51a6ee92b9
jessebrite <jessebrite@gmail.com>
2019-06-25 17:38:54 +0100

    Refactored code

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ let input = document.querySelector('input');
 loadEvents();
 
 function loadEvents() {
-	document
-					.querySelector('form')
-					.addEventListener('submit', submit);
-	document
-					.getElementById('clear')
-					.addEventListener('click', clearList);
+  // Event Listener for submit
+	document.querySelector('form').addEventListener('submit', submit);
+    // Event Listener for clearList
+	document.getElementById('clear').addEventListener('click', clearList);
+    // Event Listener for tickOrDelete
+	document.querySelector('ul').addEventListener('click', tickOrDelete);
+}
 
-	document
-					.querySelector('ul')
-					.addEventListener('click', tickOrDelete);
+function addTask(task) {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><input id="${task}" type="checkbox"><label for="${task}">${task}</label>`;
+  document.querySelector('ul').appendChild(li);
+  document.querySelector('.taskBoard').style.display = 'block';
 }
 
 // submit date function
@@ -30,18 +34,10 @@ function clearList(e) {
 
 function tickOrDelete(e) {
 	if (e.target.className === 'delete') {
-		deleteTask(e); 
+		deleteTask(e);
 	} else {
 		tickTask(e);
 	}
-}
-
-function addTask(task) {
-  let ul = document.querySelector('ul');
-  let li = document.createElement('li');
-  li.innerHTML = `<span class="delete">×</span><input id="${task}" type="checkbox"><label for="${task}">${task}</label>`;
-  document.querySelector('ul').appendChild(li);
-  document.querySelector('.taskBoard').style.display = 'block';
 }
 
 function deleteTask(e) {
